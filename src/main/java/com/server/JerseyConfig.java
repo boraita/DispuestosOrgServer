c0f7b30e0acf0cfd7c6d4c721ff23ec5cac8a296
Rafael Montaño Boraita <boraita@gmail.com>
2017-10-11 17:27:13 +0200

    Update pom

--- src/main/java/com/server/JerseyConfig.java
+++ src/main/java/com/server/JerseyConfig.java
@@ -1,19 +1,15 @@
 package com.server;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.sql.DataSource;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import com.server.example.Service;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 @Component
 public class JerseyConfig extends ResourceConfig
@@ -39,16 +35,16 @@ public class JerseyConfig extends ResourceConfig
 		}
 	}
 	
-	@Bean
-	public DataSource dataSource() throws SQLException {
-		if (dbUrl == null || dbUrl.isEmpty()) {
-			HikariConfig config = new HikariConfig();
-			return new HikariDataSource(config);
-		} else {
-			HikariConfig config = new HikariConfig();
-			config.setJdbcUrl(dbUrl);
-			return new HikariDataSource(config);
-		}
-
-	}
+//	@Bean
+//	public DataSource dataSource() throws SQLException {
+//		if (dbUrl == null || dbUrl.isEmpty()) {
+//			HikariConfig config = new HikariConfig();
+//			return new HikariDataSource(config);
+//		} else {
+//			HikariConfig config = new HikariConfig();
+//			config.setJdbcUrl(dbUrl);
+//			return new HikariDataSource(config);
+//		}
+//
+//	}
 }
